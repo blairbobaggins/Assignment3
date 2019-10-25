26adfd62bc36b1b6f642225807c5ddb2266b228c
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2019-10-25 19:51:15 -0400

    Revert "zombie lerping to a destination"
    This reverts commit 7de690d1d3cfdac68560ee96c78edee5e2ed095e.

--- src/SpawnZombie.cpp
+++ src/SpawnZombie.cpp
@@ -13,39 +13,45 @@ void SpawnZombie::setup(const ofxBox2d &worldref)
 {
     m_ZombieImage.loadImage("Images/zombie2.png");
     m_ZombiePos.set(100, ofRandom(100, 500));
-    
+    endpoint.set(1000, m_ZombiePos.y);
+
+	//collisionchecker = false;
+
 
 
     world = worldref;
 
+
+
+    //temppos = world.toB2d(m_ZombiePos.x, m_ZombiePos.y);
 	collisionbox.setPhysics(3.0, 0.1, 1.5);
-    collisionbox.setup(world.getWorld(), ofRandom(10, 100), ofRandom(100, 600), 91.5f, 152.0f, 0.0f);
-	collisionbox.setLinearDamping(1.0f);
-	endpoint.set(2400, collisionbox.getPosition().y);
+    collisionbox.setup(world.getWorld(), 400, 500, 91.5f, 152.0f, 0.0f);
+	
+
 }
 
 void SpawnZombie::update()
 {
     ofVec2f ZombieToEnd = endpoint - collisionbox.getPosition();
 
+
 	
 	collisionbox.update();
 	collisionbox.setRotation(0);
 	m_ZombieImage.resize(collisionbox.getWidth(), collisionbox.getHeight());
-	collisionbox.setPosition(Lerp(collisionbox.getPosition().x, ZombieToEnd.x, 0.0003f),
-		Lerp(collisionbox.getPosition().y, ZombieToEnd.y, 0.0003f));
+	//cout << collisionbox.getVelocity() << endl;
 }
 void SpawnZombie::draw()
 {
 	collisionbox.draw();
     ofPushMatrix();
+        
+        //ofScale(0.3);
+		//ofRotate(collisionbox.getRotation());
 		ofTranslate(collisionbox.getPosition());
 		m_ZombieImage.draw(0,0);
     ofPopMatrix();
 	//cout << "Temp Pos " << temppos << endl;
 
 }
-float SpawnZombie::Lerp(float start, float end, float percent)
-{
-	return(start + percent * (end - start));
-}
+
